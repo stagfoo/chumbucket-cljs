822055d1ef3b3bdc8a8609299f554a34b00643d8
Alex King <alex@basal.dev>
2021-11-30 09:53:07 +0900

    🪲　bugfix |  remove space

--- dev/user.cljs
+++ dev/user.cljs
@@ -1,6 +1,6 @@
 (ns cljs.user
   "Commonly used symbols for easy access in the ClojureScript REPL during
-  development. "
+  development."
   (:require
     [cljs.repl :refer (Error->map apropos dir doc error->str ex-str ex-triage
                        find-doc print-doc pst source)]
